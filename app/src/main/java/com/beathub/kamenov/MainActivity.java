ff8e62d18240a63636fe8166afa2318b64d6b7a4
RsknCankov <rosen_cankov@mail.bg>
2014-12-30 11:05:58 +0200

    Added some comment

--- app/src/main/java/com/beathub/kamenov/MainActivity.java
+++ app/src/main/java/com/beathub/kamenov/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //test comment
+        //test comment qvno e commitnat veche
     }
 
 
