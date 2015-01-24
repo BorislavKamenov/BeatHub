dbbf191a9f04d32c326d88bad5aff82786126627
rosentsankov <rosen_cankov@mail.bg>
2015-01-24 11:32:26 +0200

    Save

--- app/src/main/java/com/beathub/kamenov/ArtcoverViewPagerAdapter.java
+++ app/src/main/java/com/beathub/kamenov/ArtcoverViewPagerAdapter.java
@@ -9,63 +9,60 @@ import android.support.v4.view.ViewPager;
 import com.nineoldandroids.view.ViewHelper;
 
 public class ArtcoverViewPagerAdapter extends FragmentPagerAdapter implements
-ViewPager.OnPageChangeListener {
+        ViewPager.OnPageChangeListener {
 
-	private boolean swipedLeft = false;
-	private int lastPage;
-	private MyLinearLayout cur = null;
-	private MyLinearLayout next = null;
-	private MyLinearLayout prev = null;
-	private MyLinearLayout prevprev = null;
-	private MyLinearLayout nextnext = null;
-	private MainArtCoverFragment context;
-	private FragmentManager fm;
-	private float scale;
-	private boolean IsBlured;
-	private static float minAlpha=0.6f;
-	private static float maxAlpha=1f;
-	private static float minDegree=60.0f;
-	private int lastPosition = -1;
+    private boolean swipedLeft = false;
+    private int lastPage;
+    private MyLinearLayout cur = null;
+    private MyLinearLayout next = null;
+    private MyLinearLayout prev = null;
+    private MyLinearLayout prevprev = null;
+    private MyLinearLayout nextnext = null;
+    private MainArtCoverFragment context;
+    private FragmentManager fm;
+    private float scale;
+    private boolean IsBlured;
+    private static float minAlpha = 0.6f;
+    private static float maxAlpha = 1f;
+    private static float minDegree = 60.0f;
+    private int lastPosition = -1;
 
-	public static float getMinDegree()
-	{
-		return minDegree;
-	}
-	public static float getMinAlpha()
-	{
-		return minAlpha;
-	}
-	public static float getMaxAlpha()
-	{
-		return maxAlpha;
-	}
-	
-	public ArtcoverViewPagerAdapter(MainArtCoverFragment context, FragmentManager fm) {
-		super(fm);
-		this.fm = fm;
-		this.context = context;
+    public static float getMinDegree() {
+        return minDegree;
+    }
+
+    public static float getMinAlpha() {
+        return minAlpha;
+    }
+
+    public static float getMaxAlpha() {
+        return maxAlpha;
+    }
+
+    public ArtcoverViewPagerAdapter(MainArtCoverFragment context, FragmentManager fm) {
+        super(fm);
+        this.fm = fm;
+        this.context = context;
 
         //initialise lastpage
-        this.lastPage = ((MainActivity)context.getActivity()).getSongList().size() - 1;
-	}
+        this.lastPage = ((MainActivity) context.getActivity()).getSongList().size() - 1;
+    }
 
-	@Override
-	public Fragment getItem(int position) 
-	{
-		// make the first pager bigger than others
-		if (position == MainArtCoverFragment.FIRST_PAGE)
-			scale = MainArtCoverFragment.BIG_SCALE;
-		else
-		{
-			scale = MainArtCoverFragment.SMALL_SCALE;
-			IsBlured=true;
+    @Override
+    public Fragment getItem(int position) {
+        // make the first pager bigger than others
+        if (position == MainArtCoverFragment.FIRST_PAGE)
+            scale = MainArtCoverFragment.BIG_SCALE;
+        else {
+            scale = MainArtCoverFragment.SMALL_SCALE;
+            IsBlured = true;
 
-		}
+        }
 
-		Fragment curFragment= ArtCoverContentFragment.newInstance(context, position, scale, IsBlured);
-		cur = getRootView(position);
-		next = getRootView(position +1);
-		prev = getRootView(position -1);
+        Fragment curFragment = ArtCoverContentFragment.newInstance(context, position, scale, IsBlured);
+        cur = getRootView(position);
+        next = getRootView(position + 1);
+        prev = getRootView(position - 1);
 
 //        if(lastPosition != position){
 //
@@ -79,124 +76,110 @@ ViewPager.OnPageChangeListener {
 //
 //        lastPosition = position;
 
-		return curFragment;
-	}
+        return curFragment;
+    }
 
-	@Override
-	public int getCount()
-	{
-		return ((MainActivity)context.getActivity()).getSongList().size();
-	}
+    @Override
+    public int getCount() {
+        return ((MainActivity) context.getActivity()).getSongList().size();
+    }
 
-	@Override
-	public void onPageScrolled(int position, float positionOffset,int positionOffsetPixels){
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
-		if (positionOffset >= 0f && positionOffset <= 1f)
-		{
-			positionOffset = positionOffset * positionOffset;
-			cur = getRootView(position);
-			next = getRootView(position +1);
-			prev = getRootView(position -1);
-			nextnext=getRootView(position +2);
+        if (positionOffset >= 0f && positionOffset <= 1f) {
+            positionOffset = positionOffset * positionOffset;
+            cur = getRootView(position);
+            next = getRootView(position + 1);
+            prev = getRootView(position - 1);
+            nextnext = getRootView(position + 2);
 
-			ViewHelper.setAlpha(cur, maxAlpha-0.5f*positionOffset);
-			ViewHelper.setAlpha(next, minAlpha+0.5f*positionOffset);
-			ViewHelper.setAlpha(prev, minAlpha+0.5f*positionOffset);
-			
-			
-			if(nextnext!=null)
-			{	
-				ViewHelper.setAlpha(nextnext, minAlpha);
-				ViewHelper.setRotationY(nextnext, -minDegree);
-			}
-			if(cur!=null)
-			{
-				cur.setScaleBoth(MainArtCoverFragment.BIG_SCALE
-						- MainArtCoverFragment.DIFF_SCALE * positionOffset);
+            ViewHelper.setAlpha(cur, maxAlpha - 0.5f * positionOffset);
+            ViewHelper.setAlpha(next, minAlpha + 0.5f * positionOffset);
+            ViewHelper.setAlpha(prev, minAlpha + 0.5f * positionOffset);
 
-				ViewHelper.setRotationY(cur, 0);
-			}
 
-			if(next!=null)
-			{
-				next.setScaleBoth(MainArtCoverFragment.SMALL_SCALE
-						+ MainArtCoverFragment.DIFF_SCALE * positionOffset);
-				ViewHelper.setRotationY(next, -minDegree);
-			}
-			if(prev!=null)
-			{
-				ViewHelper.setRotationY(prev, minDegree);
-			}
+            if (nextnext != null) {
+                ViewHelper.setAlpha(nextnext, minAlpha);
+                ViewHelper.setRotationY(nextnext, -minDegree);
+            }
+            if (cur != null) {
+                cur.setScaleBoth(MainArtCoverFragment.BIG_SCALE
+                        - MainArtCoverFragment.DIFF_SCALE * positionOffset);
+
+                ViewHelper.setRotationY(cur, 0);
+            }
+
+            if (next != null) {
+                next.setScaleBoth(MainArtCoverFragment.SMALL_SCALE
+                        + MainArtCoverFragment.DIFF_SCALE * positionOffset);
+                ViewHelper.setRotationY(next, -minDegree);
+            }
+            if (prev != null) {
+                ViewHelper.setRotationY(prev, minDegree);
+            }
 
 
 			/*To animate it properly we must understand swipe direction
-			 * this code adjusts the rotation according to direction.
+             * this code adjusts the rotation according to direction.
 			 */
-			if(swipedLeft)
-			{
-				if(next!=null)
-					ViewHelper.setRotationY(next, -minDegree+minDegree*positionOffset);
-				if(cur!=null)
-					ViewHelper.setRotationY(cur, 0+minDegree*positionOffset);
+            if (swipedLeft) {
+                if (next != null)
+                    ViewHelper.setRotationY(next, -minDegree + minDegree * positionOffset);
+                if (cur != null)
+                    ViewHelper.setRotationY(cur, 0 + minDegree * positionOffset);
 
-			}
-			else 
-			{
-				if(next!=null)
-					ViewHelper.setRotationY(next, -minDegree+minDegree*positionOffset);
-				if(cur!=null)
-				{
-					ViewHelper.setRotationY(cur, 0+minDegree*positionOffset);
-				}
+            } else {
+                if (next != null)
+                    ViewHelper.setRotationY(next, -minDegree + minDegree * positionOffset);
+                if (cur != null) {
+                    ViewHelper.setRotationY(cur, 0 + minDegree * positionOffset);
+                }
 
-			}
-		}
-		if(positionOffset>=1f)
-		{
-			ViewHelper.setAlpha(cur, maxAlpha);
-		}
-	}
+            }
+        }
+        if (positionOffset >= 1f) {
+            ViewHelper.setAlpha(cur, maxAlpha);
+        }
+    }
 
-	@Override
-	public void onPageSelected(int position) {
+    @Override
+    public void onPageSelected(int position) {
 
 /*
  * to get finger swipe direction
  */
-		if(lastPage <= position)
-		{
-			swipedLeft = true;
+        if (lastPage <= position) {
+            swipedLeft = true;
 
-		}
-		else if(lastPage > position)
-		{
-			swipedLeft = false;
-		}
-		lastPage = position;
+        } else if (lastPage > position) {
+            swipedLeft = false;
+        }
+        lastPage = position;
 
-	}
+    }
 
-	@Override
-	public void onPageScrollStateChanged(int state) {
-	}
+    @Override
+    public void onPageScrollStateChanged(int state) {
+    }
 
-	private MyLinearLayout getRootView(int position){
+    private MyLinearLayout getRootView(int position) {
 
-		MyLinearLayout ly;
-		try {
-			ly = (MyLinearLayout) 
-					fm.findFragmentByTag(this.getFragmentTag(position)).getView().findViewById(R.id.animated_view_pager);
-		} catch (Exception e) {
+        MyLinearLayout ly;
+        try {
+            ly = (MyLinearLayout)
+                    fm.findFragmentByTag(this.getFragmentTag(position)).getView().findViewById(R.id.animated_view_pager);
+        } catch (Exception e) {
 
-			return null;
-		}
-		if(ly!=null)
-			return ly;
-		return null;
-	}
+            return null;
+        }
+        if (ly != null)
+            return ly;
+        return null;
+    }
 
-	private String getFragmentTag(int position){
+    private String getFragmentTag(int position) {
 
-		return "android:switcher:" + context.viewPager.getId() + ":" + position;
-	}
+        return "android:switcher:" + context.viewPager.getId() + ":" + position;
+    }
 }
